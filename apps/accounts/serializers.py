2241b2bdfa39ad1117e301038ada42a57691750b
Afnitha <afnitharishad25@gmail.com>
2026-08-02 15:14:17 +0400

    Update user manager and account serializers

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -4,6 +4,7 @@ from .models import User
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
+    confirm_password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
@@ -17,13 +18,23 @@ class RegisterSerializer(serializers.ModelSerializer):
             "industry_type",
             "country",
             "password",
+            "confirm_password",
         ]
+    
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {"confirm_password": "Passwords do not match."}
+            )
+        return attrs
 
+    
     def create(self, validated_data):
+        validated_data.pop("confirm_password")
         password = validated_data.pop("password")
 
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-
-        return user
+       
+        return User.objects.create_user(
+        password=password,
+        **validated_data,
+    )

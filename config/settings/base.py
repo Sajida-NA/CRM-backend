7ece1c4c38541f5ff5a42060898ce3eee6597972
Sibin P <puthramannilshibin@gmail.com>
2026-08-05 18:17:52 +0530

    Initial project setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,17 +100,17 @@ DATABASES = {
     }
 }
 
-#  Email Configuration
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# #  Email Configuration
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+# EMAIL_HOST = config("EMAIL_HOST")
+# EMAIL_PORT = config("EMAIL_PORT", cast=int)
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+# DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
-FRONTEND_URL = config("FRONTEND_URL")
+# FRONTEND_URL = config("FRONTEND_URL")
 
 
 # Password validation

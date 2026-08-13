25d643c95aed0857cb7ef9c35a51219bd599f96d
Sibin P <puthramannilshibin@gmail.com>
2026-08-13 14:18:32 +0530

    Add reusable activities module

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/accounts/", include("apps.accounts.urls")),
     path("api/", include("apps.companies.urls")),
+    path("api/activities/", include("apps.activities.urls")),
 
 
     path("api/token/",TokenObtainPairView.as_view(), name="token_obtain_pair",),
@@ -33,4 +34,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh",),
 
     path("api/test/", TestView.as_view()),
+    
 ]

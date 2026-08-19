b1d71656e9ce56429a0ba5ad2c43ba9c242e0e48
Sibin P <puthramannilshibin@gmail.com>
2026-08-19 11:45:24 +0530

    Update company fields to match frontend

--- apps/companies/admin.py
+++ apps/companies/admin.py
@@ -1,29 +1,39 @@
 from django.contrib import admin
+
 from .models import Company
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
+
     list_display = (
         "id",
-        "name",
-        "email",
-        "phone_number",
+        "company_name",
+        "domain_name",
         "industry",
-        "owner",
+        "type",
+        "company_owner",
+        "city",
+        "country_region",
+        "no_of_employees",
+        "annual_revenue",
+        "phone_number",
+        "email",
         "lifecycle_stage",
         "created_at",
     )
 
     search_fields = (
-        "name",
+        "company_name",
+        "domain_name",
         "email",
         "phone_number",
+        "industry",
     )
 
     list_filter = (
         "industry",
+        "type",
+        "country_region",
         "lifecycle_stage",
     )
-
-# Register your models here.

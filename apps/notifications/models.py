2805a43327c1cfb113dd0d0abe0dbfe9755df14b
Afnitha <afnitharishad25@gmail.com>
2026-09-09 17:48:58 +0400

    Add redirect URL to notifications

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -11,6 +11,7 @@ class Notification(models.Model):
     )
     title = models.CharField(max_length=255)
     message = models.TextField()
+    redirect_url = models.CharField(max_length=255,blank=True,null=True,)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 

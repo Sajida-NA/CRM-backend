159f0943dfada58bea592bb410cb7399ace9e505
sajida <sajijubi2423@gmail.com>
2026-09-21 17:50:29 +0400

    Fix Email

--- apps/activities/email/urls.py
+++ apps/activities/email/urls.py
@@ -1,5 +1,54 @@
     
 
+# from django.urls import path
+
+# from .views import (
+#     EmailListCreateView,
+#     EmailDetailView,
+#     EmailRecipientView,
+# )
+
+
+# urlpatterns = [
+
+#     # ==========================================
+#     # EMAIL LIST + CREATE
+#     # ==========================================
+
+#     path(
+#         "",
+#         EmailListCreateView.as_view(),
+#         name="email-list-create",
+#     ),
+
+
+#     # ==========================================
+#     # GET AUTOMATIC RECIPIENT
+#     # IMPORTANT: Put this BEFORE <int:pk>/
+#     # ==========================================
+
+#     path(
+#         "recipient/<str:module>/<int:object_id>/",
+#         EmailRecipientView.as_view(),
+#         name="email-recipient",
+#     ),
+
+
+#     # ==========================================
+#     # EMAIL DETAIL
+#     # ==========================================
+
+#     path(
+#         "<int:pk>/",
+#         EmailDetailView.as_view(),
+#         name="email-detail",
+#     ),
+
+# ]
+
+
+
+
 from django.urls import path
 
 from .views import (
@@ -24,11 +73,10 @@ urlpatterns = [
 
     # ==========================================
     # GET AUTOMATIC RECIPIENT
-    # IMPORTANT: Put this BEFORE <int:pk>/
     # ==========================================
 
     path(
-        "recipient/<str:module>/<int:object_id>/",
+        "recipient/",
         EmailRecipientView.as_view(),
         name="email-recipient",
     ),
@@ -45,3 +93,5 @@ urlpatterns = [
     ),
 
 ]
+
+
